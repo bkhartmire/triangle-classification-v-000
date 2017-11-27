137711fcfce663e40ea4d8bd4d02c0d99f36735c
Brittany Hartmire <bkhartmire@gmail.com>
2017-11-27 23:21:04 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,9 +1,13 @@
 class Triangle
+  attr_reader :side_one, :side_two, :side_three
   def initialize(side_one, side_two, side_three)
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three
   end
 
   def kind
-    
+
   end
 end
 
