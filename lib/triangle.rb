ff593d0c7a553e865fe73b7f5155d72a8faf2c99
Brittany Hartmire <bkhartmire@gmail.com>
2017-11-27 23:27:36 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,10 +7,16 @@ class Triangle
   end
 
   def kind
-
+    sides = [self.side_one, self.side_two, self.side_three]
+    sides.sort!
+    if sides.any? {|side| side <= 0} || sides[0] + sides[1] < sides[2]
+      raise TriangleError
+    elsif self.side_one == self.side_two && self.side_two == self.side_three
+      :equilateral
+    elsif self.side_one
   end
 end
 
 class TriangleError < StandardError
-  
+
 end
